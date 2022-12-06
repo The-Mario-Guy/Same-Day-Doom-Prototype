e8998358e17c6f5817aa17712c887153302ad5b9
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2022-12-06 11:04:26 -0800

    Attempted to do Running

--- Same Day Doom/Assets/Scripts/TransformMovement.cs
+++ Same Day Doom/Assets/Scripts/TransformMovement.cs
@@ -6,6 +6,11 @@ public class TransformMovement : MonoBehaviour
 {
     public float speed = 3;
     public float run = 2;
+    public bool isRunning = false;
+    public bool isMoving = false;
+    public bool isIdle = true;
+    private Animator _playerAnim;
+    private Rigidbody2D _playerRB;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +21,31 @@ public class TransformMovement : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+       _playerAnim = GetComponent<Animator>();
+       _playerRB = GetComponent<Rigidbody2D>();
     }
     void PlayerMovement()
     {
+        isMoving = true;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
 
         transform.Translate(Vector2.up * verticalInput * speed * Time.deltaTime);
+       //Running
+        if(Input.GetKey(KeyCode.Z))
+        {
+            isRunning = true;
+            isMoving = false;
+            _playerAnim.SetBool("isRunning", isRunning);
+            speed = 5;
+        }
+        else
+        {   isRunning = false;
+            isMoving = true;
+            speed = 3;
+        }
+
     }
-    
 }
